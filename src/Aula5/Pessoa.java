d847796ae29b406b35214fc5003bc61969fa059e
gabrieldelfino28 <gabrieldelfino28@gmail.com>
2019-04-19 06:39:11 -0300

    aula5

---
+++ src/Aula5/Pessoa.java
@@ -0,0 +1,14 @@
+package Aula5;
+
+public class Pessoa {
+	public void andar() {
+		System.out.println("Andando");
+	}
+	protected void respirar() {
+		System.out.println("Respirando");
+	}
+	private void falar() {
+		System.out.println("Falando");
+	}
+
+}
---
+++ src/exec/Execução.java
@@ -0,0 +1,14 @@
+package exec;
+
+import Aula5.Pessoa;
+
+public class Execução {
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		Pessoa ps = new Pessoa ();
+		ps.andar();
+		
+	}
+
+}
